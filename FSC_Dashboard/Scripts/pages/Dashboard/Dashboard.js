b43b4883dcaaaacc4b88cb3264a2dd0f183c85f3
ajru <azharmajkure@gmail.com>
2021-08-26 21:09:21 +0530

    charts added

--- FSC_Dashboard/Scripts/pages/Dashboard/Dashboard.js
+++ FSC_Dashboard/Scripts/pages/Dashboard/Dashboard.js
@@ -16,6 +16,7 @@ $(document).ready(function (event) {
     opt_load_factor()
     disruption_chart()
     delay_analysis_chart()
+    plannedVsActual_chart()
 });
 
 
@@ -1037,6 +1038,122 @@ function otp_metro_chart() {
     });
 }
 
+function opt_load_factor() {
+    Highcharts.chart('meter_load_factor',
+        {
+            chart: {
+                marginTop: -100,
+                type: 'gauge',
+                backgroundColor: 'transparent',
+                alignTicks: false,
+                plotBackgroundColor: null,
+                plotBackgroundImage: null,
+                plotBorderWidth: 0,
+                plotShadow: false
+            },
+
+            title: {
+                text: 'Load Factor',
+                style: {
+                    color: '#eee'
+                },
+            },
+            exporting: false,
+            credits: false,
+
+            pane: {
+                startAngle: -150,
+                endAngle: 150,
+                size:190
+            },
+
+            yAxis: [{
+                min: 0,
+                max: 200,
+                lineColor: '#339',
+                tickColor: '#339',
+                minorTickColor: '#339',
+                offset: -25,
+                lineWidth: 2,
+                labels: {
+                    distance: -10,
+                    rotation: 'auto'
+                },
+                tickLength: 5,
+                minorTickLength: 5,
+                endOnTick: false
+            }, {
+                min: 0,
+                max: 124,
+                tickPosition: 'outside',
+                lineColor: '#933',
+                lineWidth: 2,
+                minorTickPosition: 'outside',
+                tickColor: '#933',
+                minorTickColor: '#933',
+                tickLength: 5,
+                minorTickLength: 5,
+                labels: {
+                    distance: 12,
+                    rotation: 'auto'
+                },
+                offset: -20,
+                endOnTick: false
+            }],
+
+            series: [{
+                name: 'Speed',
+                data: [80],
+                dataLabels: {
+                    formatter: function () {
+                        var kmh = this.y,
+                            mph = Math.round(kmh * 0.621);
+                        return '<span style="color:#339">' + kmh + ' km/h</span><br/>' +
+                            '<span style="color:#933">' + mph + ' mph</span>';
+                    },
+                    backgroundColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, '#DDD'],
+                            [1, '#FFF']
+                        ]
+                    }
+                },
+                tooltip: {
+                    valueSuffix: ' km/h'
+                }
+            }]
+
+        },
+        // Add some life
+        function (chart) {
+            setInterval(function () {
+                if (chart.axes) { // not destroyed
+                    var point = chart.series[0].points[0],
+                        newVal,
+                        inc = Math.round((Math.random() - 0.5) * 20);
+
+                    newVal = point.y + inc;
+                    if (newVal < 0 || newVal > 200) {
+                        newVal = point.y - inc;
+                    }
+
+                    point.update(newVal);
+                }
+            }, 3000);
+
+        });
+
+}
+
+
+// Load factor
+
 function disruption_chart() {
 
     Highcharts.chart('bar_disruption', {
@@ -1044,9 +1161,9 @@ function disruption_chart() {
             type: 'column',
             backgroundColor: 'transparent',
         },
-        title: {
-            text: 'Stacked column chart'
-        },
+        title: false,
+        exporting: false,
+        credits: false,
         xAxis: {
             categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
         },
@@ -1082,6 +1199,7 @@ function delay_analysis_chart() {
     var chart = new Highcharts.Chart({
         chart: {
             renderTo: 'delay_analysis',
+            marginTop: -100,
             type: 'pie',
             backgroundColor: 'transparent',
             events: {
@@ -1117,18 +1235,18 @@ function delay_analysis_chart() {
                 }
             }
         },
+        title: false,
+        exporting: false,
+        credits: false,
         plotOptions: {
             pie: {
                 innerSize: '60%'
             }
         },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'middle'
-        },
+        legend:false,
         series: [{
             showInLegend: true,
+            size: '34%',
             data: [
                 ['Firefox', 44.2],
                 ['IE7', 26.6],
@@ -1140,110 +1258,33 @@ function delay_analysis_chart() {
     });
 }
 
-function opt_load_factor() {
-    Highcharts.chart('meter_load_factor',
-        {
+function plannedVsActual_chart() {
 
+    Highcharts.chart('plannedVsActual', {
         chart: {
-            type: 'gauge',
-            backgroundColor: 'transparent',
-            alignTicks: false,
-            plotBackgroundColor: null,
-            plotBackgroundImage: null,
-            plotBorderWidth: 0,
-            plotShadow: false
+            type: 'column',
+            backgroundColor: 'transparent'    
         },
-
-        title: {
-            text: 'Load Factor'
+        exporting: false,
+        credits: false,
+        title: false,
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
         },
-
-        pane: {
-            startAngle: -150,
-            endAngle: 150
+        credits: {
+            enabled: false
         },
-
-        yAxis: [{
-            min: 0,
-            max: 200,
-            lineColor: '#339',
-            tickColor: '#339',
-            minorTickColor: '#339',
-            offset: -25,
-            lineWidth: 2,
-            labels: {
-                distance: -20,
-                rotation: 'auto'
-            },
-            tickLength: 5,
-            minorTickLength: 5,
-            endOnTick: false
-        }, {
-            min: 0,
-            max: 124,
-            tickPosition: 'outside',
-            lineColor: '#933',
-            lineWidth: 2,
-            minorTickPosition: 'outside',
-            tickColor: '#933',
-            minorTickColor: '#933',
-            tickLength: 5,
-            minorTickLength: 5,
-            labels: {
-                distance: 12,
-                rotation: 'auto'
-            },
-            offset: -20,
-            endOnTick: false
-        }],
-
         series: [{
-            name: 'Speed',
-            data: [80],
-            dataLabels: {
-                formatter: function () {
-                    var kmh = this.y,
-                        mph = Math.round(kmh * 0.621);
-                    return '<span style="color:#339">' + kmh + ' km/h</span><br/>' +
-                        '<span style="color:#933">' + mph + ' mph</span>';
-                },
-                backgroundColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, '#DDD'],
-                        [1, '#FFF']
-                    ]
-                }
-            },
-            tooltip: {
-                valueSuffix: ' km/h'
-            }
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: 'Jane',
+            data: [2, -2, -3, 2, 1]
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, -2, 5]
         }]
+    });
 
-    },
-        // Add some life
-        function (chart)
-        {
-            setInterval(function () {
-                if (chart.axes) { // not destroyed
-                    var point = chart.series[0].points[0],
-                        newVal,
-                        inc = Math.round((Math.random() - 0.5) * 20);
-
-                    newVal = point.y + inc;
-                    if (newVal < 0 || newVal > 200) {
-                        newVal = point.y - inc;
-                    }
-
-                    point.update(newVal);
-                }
-            }, 3000);
-
-        });
 
 }
